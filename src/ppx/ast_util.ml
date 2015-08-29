5183cd44e15066e198ba21bfbbbd2959aa68a377
derui <derutakayu@gmail.com>
2015-08-29 17:09:13 +0900

    Add main converter for ppx.

---
+++ src/ppx/ast_util.ml
@@ -0,0 +1,58 @@
+open Ast_helper
+open Ast_mapper
+open Asttypes
+open Parsetree
+open Longident
+open Location
+
+let may_tuple tup = function
+  | [] -> None
+  | [x] -> Some x
+  | l -> Some (tup ?loc:None ?attrs:None l)
+
+let lid s = mkloc (Longident.parse s) !default_loc
+let constr s args = Exp.construct (lid s) (may_tuple Exp.tuple args)
+let nil () = constr "[]" []
+let cons hd tl = constr "::" [hd; tl]
+let list l = List.fold_right cons l (nil ())
+
+let pconstr s args = Pat.construct (lid s) (may_tuple Pat.tuple args)
+let pnil () = pconstr "[]" []
+let pcons hd tl = pconstr "::" [hd;tl]
+let plist l = List.fold_right pcons l (pnil ())
+
+(* Convert (e1,e2,e3,...) to [e1;E2;e3;..] *)
+let tup_to_list tup =
+  let rec tup_to_list' memo = function
+    | {pexp_desc = Pexp_construct ({txt = Lident "[]";_}, None)} -> List.rev memo
+    | {pexp_desc = Pexp_construct ({txt = Lident "::";_}, Some({pexp_desc = Pexp_tuple [hd;tl];_}))} ->
+       tup_to_list' (hd :: memo) tl
+    | _ -> failwith "This expression is not expression to construct list."
+  in
+  tup_to_list' [] tup
+
+(* Shortcut for [fun a b... -> e] as [fun_ ["a";"b"] e]*)
+let fun_ args e =
+  match args with
+  | [] -> failwith "Invalid patterns to Util.fun_, only accept n >= 1"
+  | pat :: rest ->
+     let rec fun_' pats func =
+       match pats with
+       | [] -> func
+       | pat :: rest -> fun_' rest (Exp.fun_ "" None (Pat.var {txt = pat;loc = !default_loc}) func) in
+     fun_' rest (Exp.fun_ "" None (Pat.var {txt = pat;loc = !default_loc}) e)
+
+let names_to_module_path paths =
+  match paths with
+  | [] -> failwith "need least one path to resolve module path"
+  | f :: paths ->
+    List.fold_left (fun ident path -> Ldot (ident, path)) (Lident f) paths
+
+let to_ounit_fun ?(loc=Location.none) name = Exp.ident {txt = names_to_module_path ["OUnit2";name];loc}
+
+module Exp = struct
+  let is_ident = function
+    | {pexp_desc = Pexp_ident _;_} -> true
+    | _ -> false
+  (* Check a expression is either identity or not *)
+end
---
+++ src/ppx/main.ml
@@ -0,0 +1,77 @@
+open Core.Std
+open Ast_helper
+open Ast_mapper
+open Asttypes
+open Parsetree
+open Longident
+open Location
+
+exception Error of Location.t * string
+
+let () =
+  Location.register_error_of_exn (fun exn ->
+    match exn with
+    | Error (loc, message) ->
+      Some (error ~loc message)
+    | _ -> None)
+
+let attribute_to_vec ~loc = function
+  | PStr [{pstr_desc = Pstr_eval (e, _);_}] -> begin
+    match e with
+    | {pexp_desc = Pexp_construct ({txt = Lident "::";_}, Some ({pexp_desc = Pexp_tuple list;_}));_} -> begin
+      failwith (Printf.sprintf "%d^n" (List.length list))
+    end
+    | _ -> failwith (Printf.sprintf "@vec have to be contained list as vector.")
+  end
+  | _ -> failwith (Printf.sprintf "@vec have to be contained list as vector.")
+
+let attribute_to_mat ~loc = function
+  | PStr [{pstr_desc = Pstr_eval (e, _);_}] -> begin
+    match e with
+    | {pexp_desc = Pexp_construct ({txt = Lident "::";_}, Some ({pexp_desc = Pexp_tuple list;_}));_} -> begin
+      failwith (Printf.sprintf "%d^n" (List.length list))
+    end
+    | _ -> failwith (Printf.sprintf "@vec have to be contained list as vector.")
+  end
+  | _ -> failwith (Printf.sprintf "@vec have to be contained list as vector.")
+
+
+let expand_vec ~loc = function
+  | PStr [{pstr_desc = Pstr_eval (e,_);_}] -> begin
+    match e with
+    | {pexp_desc = Pexp_construct ({txt = Lident "::";_}, _);_} ->
+       Some [%expr
+             let list = Ast_util.tup_to_list e in
+             let module S = (val Size.of_int (List.length list)) in
+             Vec.make S.size (List.hd_exn list)
+            ]
+    | {pexp_desc = Pexp_construct ({txt = Lident "[]";_},_);_} ->
+       failwith "%vec have to apply a list is contained least one element"
+    | _ -> failwith "%vec have to be applied with a list of any element."
+  end
+  | _ -> None
+
+(* Convert attributes to program to generate to make vector or matrix. *)
+let convert_extensions ~loc vbs =
+  List.fold vbs ~f:(fun memo vb ->
+    match vb with
+    | {pvb_expr = {pexp_desc = Pexp_extension ({txt="vec";loc}, payload);_};_} -> begin
+      match expand_vec ~loc payload with
+      | Some e -> {vb with pvb_expr = e} :: memo
+      | None -> memo
+    end
+    | _ -> memo
+  ) ~init:[] |> List.rev
+
+(* Mapper to reconstruct for assertions that are change to some assertion method *)
+let rec assertion_mapper argv = {default_mapper with
+  expr = fun mapper expr ->
+    match expr with
+    | {pexp_desc = Pexp_let (rec_flag, vbs, e);pexp_loc = loc;pexp_attributes = attrs} ->
+       let expanded = convert_extensions ~loc vbs in
+       Printf.printf "foobar";
+       Exp.let_ ~loc ~attrs rec_flag expanded e
+    | _ -> default_mapper.expr mapper expr
+}
+
+let () = run_main assertion_mapper
