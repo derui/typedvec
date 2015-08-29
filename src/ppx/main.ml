234a36be0426c1955b764111dbd5fb8bc2d2bfa5
derui <derutakayu@gmail.com>
2015-08-29 20:04:42 +0900

    Fix generation of vector.
    [%vec] syntax can place it in any expression.

--- src/ppx/main.ml
+++ src/ppx/main.ml
@@ -11,8 +11,15 @@ let () =
   Location.register_error_of_exn (fun exn ->
     match exn with
     | Error (loc, message) ->
-      Some (error ~loc message)
+       Some (error ~loc message)
     | _ -> None)
+
+let make_size_type list =
+  let rec succ' typ = function
+    | [] -> typ
+    | v :: rest -> succ' ([%expr Typedvec.Std.Size.succ [%e typ]]) rest
+  in
+  succ' [%expr Typedvec.Std.Size.zero] list
 
 (* Expand [%vec] extension to an expression what make vector. *)
 let expand_vec ~loc = function
@@ -22,12 +29,12 @@ let expand_vec ~loc = function
        let list = Ast_util.tup_to_list e in
        let tupled_list = Ast_util.list list in
        let hd = List.hd list in
-       let len = Exp.constant (Const_int (List.length list)) in
+       let size = make_size_type list in
+       (* Expand expr using metaquot. *)
        Some [%expr
-             let module S = (val Typedvec.Std.Size.of_int [%e len]) in
-                 let v = Typedvec.Std.Vec.make S.size [%e hd] in
-                 List.iteri (fun index v -> Typedvec.Std.Vec.set v ~index ~v) [%e tupled_list];
-                 v
+             let vec = Typedvec.Std.Algebra.Vec.make [%e size] [%e hd] in
+             List.iteri (fun index v -> Typedvec.Std.Algebra.Vec.set vec ~index ~v) [%e tupled_list];
+             vec
             ]
     | {pexp_desc = Pexp_construct ({txt = Lident "[]";_},_);_} ->
        failwith "%vec have to apply a list is contained least one element"
@@ -35,26 +42,16 @@ let expand_vec ~loc = function
   end
   | _ -> None
 
-(* Convert attributes to program to generate to make vector or matrix. *)
-let convert_extensions ~loc vbs =
-  List.fold_left (fun memo vb ->
-    match vb with
-    | {pvb_expr = {pexp_desc = Pexp_extension ({txt="vec";loc}, payload);_};_} -> begin
-      match expand_vec ~loc payload with
-      | Some e -> {vb with pvb_expr = e} :: memo
-      | None -> memo
-    end
-    | _ -> memo
-  ) [] vbs |> List.rev
-
 (* Mapper to reconstruct for assertions that are change to some assertion method *)
-let rec assertion_mapper argv = {default_mapper with
+let rec extension_mapper argv = {default_mapper with
   expr = fun mapper expr ->
     match expr with
-    | {pexp_desc = Pexp_let (rec_flag, vbs, e);pexp_loc = loc;pexp_attributes = attrs} ->
-       let expanded = convert_extensions ~loc vbs in
-       Exp.let_ ~loc ~attrs rec_flag expanded e
+    | {pexp_desc = Pexp_extension ({txt="vec";loc}, payload);_} -> begin
+      match expand_vec ~loc payload with
+      | Some e -> e
+      | None -> default_mapper.expr mapper expr
+    end
     | _ -> default_mapper.expr mapper expr
 }
 
-let () = run_main assertion_mapper
+let () = run_main extension_mapper
