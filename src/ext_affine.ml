7e3f5c75c2fd0fa5bfbf816aea6d43bd15d36f7c
derui <derutakayu@gmail.com>
2015-09-27 11:03:28 +0900

    Rename and add functions of Affine module.

--- src/ext_affine.ml
+++ src/ext_affine.ml
@@ -3,7 +3,7 @@ module A = Algebra
 module Vec = Algebra_vec
 module Mat = Algebra_mat
 
-let translation ?(dir=`Row) v =
+let translation_to_mat ?(dir=`Row) v =
   let s = Vec.size v in
   let size = Size.to_int s in
   let m = Mat.identity s in
@@ -26,3 +26,27 @@ let translation ?(dir=`Row) v =
     end
   end;
   m
+
+let translation_of_mat ?(dir=`Row) m =
+  let s = Mat.row_size m in
+  let size = Size.to_int s in
+  let v = Vec.zero s in
+  let range = List.range 0 size in
+  begin
+    match dir with
+    | `Row -> begin
+      List.iter range ~f:(fun index ->
+        match Mat.get ~row:(size - 1) ~col:index m with
+        | Some v' -> Vec.set ~index ~v:v' v
+        | None -> ()
+      )
+    end
+    | `Col -> begin
+      List.iter range ~f:(fun index ->
+        match Mat.get ~col:(size - 1) ~row:index m with
+        | Some v' -> Vec.set ~index ~v:v' v
+        | None -> ()
+      )
+    end
+  end;
+  v
--- src/ext_affine.mli
+++ src/ext_affine.mli
@@ -7,11 +7,19 @@
     @version 0.1
 *)
 
-val translation : ?dir:[`Row | `Col] -> ('s Algebra.s) Algebra.vec -> ('s Algebra.s, 's Algebra.s) Algebra.mat
-(** [transiation vec] gets the Translation matrix is moved to [vec]. What default argument [~dir] is [`Row].
-    [transiation ~dir:`Row vec] gets the Translation matrix to multiply row vector.
-    [transiation ~dir:`Col vec] gets the Translation matrix to multiply column vector.
+val translation_to_mat : ?dir:[`Row | `Col] -> ('s Algebra.s) Algebra.vec -> ('s Algebra.s, 's Algebra.s) Algebra.mat
+(** [transiation_to_mat vec] gets the Translation matrix is moved to [vec]. What default argument [~dir] is [`Row].
+    [transiation_to_mat ~dir:`Row vec] gets the Translation matrix to multiply row vector.
+    [transiation_to_mat ~dir:`Col vec] gets the Translation matrix to multiply column vector.
 
     Notice: the [vec] should be a homogeneous coordinate.
 *)
 
+val translation_of_mat: ?dir:[`Row | `Col] -> ('s Algebra.s, 's Algebra.s) Algebra.mat ->
+  ('s Algebra.s) Algebra.vec
+(** [translation_of_mat ?dir mat] get the vector of translation part of the matrix.
+    What default argument [?dir] is [`Row].
+
+    [translation_of_mat ~dir:`Row mat] get bottom-edge row vector.
+    [translation_of_mat ~dir:`Col mat] get right-edge column vector.
+*)
